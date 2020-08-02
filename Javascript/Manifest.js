8baabb2befb7d7a807bc8976f6fb3cf9a149e109
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-08-02 19:35:18 -0400

    js_update

--- Javascript/Manifest.js
+++ Javascript/Manifest.js
@@ -4,11 +4,27 @@ window.addEventListener("load", () => {
 
     const pads = document.querySelectorAll(".pads div");
 
+    // grabbing the visual div
+    const visual = document.querySelector(".visual");
+
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "magenta",
+        "peru",
+        "orange",
+        "turquoise"
+    ];
 
     // sound here
+    // index is the number for each sound ,being acessed with "sounds"
 
     pads.forEach((pad, index) => {
         pad.addEventListener("click", function () {
+
+            // reset the current click sounds
+            sounds[index].currentTime = 0;
+
             sounds[index].play();
         });
     });
