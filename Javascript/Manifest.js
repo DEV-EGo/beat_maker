406c37b8cc2ad687e642ef0b338136446765a43f
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-08-03 00:30:24 -0400

    bubbles

--- Javascript/Manifest.js
+++ Javascript/Manifest.js
@@ -7,6 +7,7 @@ window.addEventListener("load", () => {
     // grabbing the visual div
     const visual = document.querySelector(".visual");
 
+    // grabbing colors of pad from style-css
     const colors = [
         "#60d394",
         "#d36060",
@@ -20,13 +21,25 @@ window.addEventListener("load", () => {
     // index is the number for each sound ,being acessed with "sounds"
 
     pads.forEach((pad, index) => {
+
         pad.addEventListener("click", function () {
 
             // reset the current click sounds
             sounds[index].currentTime = 0;
 
             sounds[index].play();
+
+            createBubbles(index);
         });
     });
 
+
+    // function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+
+        bubble.style.animation = "jump is ease";
+    };
 });
